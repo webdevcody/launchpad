ccb7258775e19852ca55ab5d9fe1ebb217a9b5d5
Web Dev Cody <webdevcody@gmail.com>
2023-04-03 22:48:09 -0400

    updating setup script to run prisma for sqlite

--- packages/create/setup.sh
+++ packages/create/setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 npm init -f
-echo "a"
 npm i @webdevcody/create-launchpad
-echo "b"
 cp -R ./node_modules/@webdevcody/create-launchpad/* .
-echo "c"
 cp -R ./node_modules/@webdevcody/create-launchpad/. .
-echo "d"
 cp .env.sample .env
-echo "e"
 npm i --save @webdevcody/shuttle@latest
-echo "f"
 npm ci
+npx prisma db push
